7897218ccfd9237042fd2182a5012ff6c0e7d207
Kevin Aizic <kevinaizic@users.noreply.github.com>
2017-12-10 04:39:37 -0500

    Update face2pi.sh

--- face2pi.sh
+++ face2pi.sh
@@ -16,7 +16,7 @@ export PYTHONPATH=/home/pi/facenet/src #for next step
 #detect up to 4 faces per img, takes ~4sec per img plus initialization time
 python ~/facenet/src/align/align_dataset_mtcnn.py ~/datasets/$dir/raw ~/datasets/$dir/aligned --image_size 160 --margin 32 --random_order --detect_multiple_faces 4
 
-#if bounding box file empty, no faces detected (my crappy PIR false triggers a lot) 
+#if aligned folder empty, no faces were detected (my crappy PIR false triggers a lot) 
 if ls -1qA ~/datasets/$dir/aligned/img/ | grep -q .; then ! echo ~~found face/s~~; else  rm -r ~/datasets/$dir && exit 0 ; fi
 #delete new img dir and exit if it's empty
 
@@ -27,3 +27,4 @@ python ~/facenet/src/classifier.py CLASSIFY ~/datasets/$dir/aligned ~/facenet/qu
 #draw bounding boxes
 ./boxer.sh ~/datasets/$dir/aligned/bounding*
 
+#to do: optimize face detection, scrounge a resistor and capacitor for a low-pass filter on this junk PIR motion detector
