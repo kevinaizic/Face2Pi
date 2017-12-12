45841b8f210730cab64972fac31bb7e21547a3ed
Kevin Aizic <kevinaizic@users.noreply.github.com>
2017-12-12 18:09:20 -0500

    Replace PIR motion sensor with amazon dash button
    Gave up on PIR until I get a better one, this calls dashbutton.py and checks for presses of my "doritos" button, then takes a burst of 50 pictures. Every 100 pictures (ie. 2 presses/bursts) becomes a batch sent off to facenet. These values can be modified easily, and the dashbutton script can be modified to your button's MAC address and name.

--- clustercam.py
+++ clustercam.py
@@ -22,16 +22,17 @@ with picamera.PiCamera() as camera:
         #I really didn't think I would miss c++
     while True: #frosted debug flakes
         i = 0
-        while i < 300: #5min       
+        while i < 100: #2 button presses = 100 pictures       
+#        time.sleep(.5) #check 
 	    #too inaccurate, for now just take ~1 pic every second
-	    #PIR = subprocess.check_output("mosquitto_sub -t outTopic -C 1", shell=True)
-            #if "SHIT" in str(PIR): #trigger burst of 10 pics on movement (false positives expected)           
-                #for x in xrange(1,11):
-            name = strftime("%Y-%m-%dx%H_%M_%S") + '.jpg' #images named by time, cluster folder named by time every 100 images input
-            camera.capture("/home/pi/cam/img/" + name, use_video_port = False)
-            print(name + " done")
-            i += 1 #but no regrets
-            time.sleep(.5) #allows for ~1 picture per second
+	    dash = subprocess.check_output("./dashbutton.py", shell=True)
+            if "doritos" in str(dash): #trigger burst of 10 pics on movement (false positives expected)           
+                for x in xrange(1,51):
+                    name = strftime("%Y-%m-%dx%H_%M_%S") + "(" + str(x) + ")" + '.jpg' #images named by time, cluster folder named by time every 100 images input
+	            camera.capture("/home/pi/cam/img/" + name, use_video_port = False)
+        	    print(name + " done")
+        	    i += 1 #but no regrets
+#        	    time.sleep(.5) #allows for ~1 picture per second
 	#every 100 pictures send to do clustering
 	time.sleep(1) #avoid incomplete image writes
 	#run face2pi in background while continuing to fill ring buffer
